3cbfb9515f999521fe15d0dd5a9ef4e2f51f534c
Kostya <73256551+IashuraI@users.noreply.github.com>
2023-10-13 12:43:50 +0000

    fix path

--- setup-environment.js
+++ setup-environment.js
@@ -8,8 +8,9 @@ const content = process.env.EMAILJS_PUBLIC_KEY;
 
 fs.access(dir, fs.constants.F_OK, (err) => {
     try{
-        fs.writeFileSync("portfolio/src/environments/environment.prod.ts", content);
-        console.log(content);
+        fs.writeFileSync("src/environments/environment.prod.ts", content);
+        fs.writeFileSync("src/environments/environment.ts", content);
+        console.log(content, process.cwd);
     }
     catch{
         console.log("unlucky");
